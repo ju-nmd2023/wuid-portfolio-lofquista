aae2b2580c6802f61421f4d656981112e2c8aab9
lofquista <loal23gk@student.ju.se>
2023-11-20 20:19:17 +0100

    trying js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const contactForm = document.getElementById('contactForm');
+// const emailBox = document.getElementsByClassName("email-box");
+// });
+
+// contactForm.addEventListener('submit', function(event) {
+//     if (!emailBox.value.includes('@')) {
+//         alert('Your email address have to contain a @.');
+//         event.preventDefault();
+//     }
+// });
+  
