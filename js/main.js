d92133cd8a8b1eeb8fc5a1c1e21b5a814e269100
lofquista <loal23gk@student.ju.se>
2023-11-21 18:25:41 +0100

    Dark mode in javascript

--- js/main.js
+++ js/main.js
@@ -10,3 +10,35 @@
 //     }
 // });
   
+
+document.addEventListener('DOMContentLoaded', function () {
+    let darkMode = document.querySelectorAll(".darkmode-section");
+    let darkModeButton = document.getElementById("darkModeButton");
+    
+    function toggleDarkMode() {
+        darkMode.forEach(section => {
+            section.classList.toggle("dark-mode");
+        });
+
+        const icon = darkModeButton.querySelector("i");
+        const isDarkMode = darkMode[0].classList.contains("dark-mode");
+
+if (isDarkMode) {
+    icon.classList.remove("fa-moon-o");
+    icon.classList.add("fa-sun-o")
+} else {
+    icon.classList.remove("fa-sun-o");
+    icon.classList.add("fa-moon-o");
+}
+
+        localStorage.setItem("dark-mode", isDarkMode);
+    }
+    if (darkModeButton) {
+        darkModeButton.addEventListener("click", toggleDarkMode);
+    }
+
+    const savedDarkMode = localStorage.getItem("darkMode");
+    if (savedDarkMode === "true") {
+        toggleDarkMode();
+    }
+});
