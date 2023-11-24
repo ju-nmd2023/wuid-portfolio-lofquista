06c8134e5b39c11435ea0e152018d2f4af114070
lofquista <loal23gk@student.ju.se>
2023-11-24 19:09:03 +0100

    changed name

--- js/main.js
+++ js/main.js
@@ -1,37 +1,38 @@
 // Dark mode //
 
-document.addEventListener('DOMContentLoaded', function () {
-    let darkMode = document.querySelectorAll(".darkmode-section");
-    let darkModeButton = document.getElementById("darkModeButton");
-    
-    function toggleDarkMode() {
-        darkMode.forEach(section => {
-            section.classList.toggle("dark-mode");
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  let darkModeElements = document.querySelectorAll(".darkmode-section");
+  let darkModeButton = document.getElementById("darkModeButton");
 
-        const icon = darkModeButton.querySelector("i");
-        const isDarkMode = darkMode[0].classList.contains("dark-mode");
+  function toggleDarkMode() {
+    darkModeElements.forEach((section) => {
+      section.classList.toggle("dark-mode");
+    });
 
-if (isDarkMode) {
-    icon.classList.remove("fa-moon-o");
-    icon.classList.add("fa-sun-o")
-} else {
-    icon.classList.remove("fa-sun-o");
-    icon.classList.add("fa-moon-o");
-}
+    const icon = darkModeButton.querySelector("i");
+    const isDarkMode = darkModeElements[0].classList.contains("dark-mode");
 
-// Next line was adapted from https://www.w3schools.com/jsref/prop_win_localstorage.asp //
-
-        localStorage.setItem("dark-mode", isDarkMode);
-    }
-    if (darkModeButton) {
-        darkModeButton.addEventListener("click", toggleDarkMode);
+    if (isDarkMode) {
+      icon.classList.remove("fa-moon-o");
+      icon.classList.add("fa-sun-o");
+    } else {
+      icon.classList.remove("fa-sun-o");
+      icon.classList.add("fa-moon-o");
     }
 
-// Next line was adapted from https://www.w3schools.com/jsref/prop_win_localstorage.asp //
+    // Next line was adapted from https://www.w3schools.com/jsref/prop_win_localstorage.asp //
 
-    const savedDarkMode = localStorage.getItem("darkMode");
-    if (savedDarkMode === "true") {
-        toggleDarkMode();
-    }
+    localStorage.setItem("dark-mode", isDarkMode);
+  }
+  
+  if (darkModeButton) {
+    darkModeButton.addEventListener("click", toggleDarkMode);
+  }
+
+  // Next line was adapted from https://www.w3schools.com/jsref/prop_win_localstorage.asp //
+
+  const savedDarkMode = localStorage.getItem("darkMode");
+  if (savedDarkMode === "true") {
+    toggleDarkMode();
+  }
 });
